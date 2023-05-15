11534b273543f8284ab2f9f930cb9cf88db7b906
abdellimed <57357370+abdellimed@users.noreply.github.com>
2023-05-15 11:37:15 +0200

    Update service.py

--- service.py
+++ service.py
@@ -1,20 +1,11 @@
 import numpy as np
-
 import bentoml
-from bentoml.io import NumpyNdarray
 
-model_runner = bentoml.mlflow.get("projet_test:latest").to_runner()
+runner = bentoml.mlflow.get("comment-classifier:latest").to_runner()
 
-svc = bentoml.Service("projet_test_service", runners=[model_runner])
-
-input_spec = NumpyNdarray(
-    dtype="int",
-    enforce_dtype=True,
-    shape=(-1, 6),
-    enforce_shape=True,
-)
+svc = bentoml.Service('comment-classifier', runners=[runner])
 
 
-@svc.api(input=input_spec, output=NumpyNdarray())
-async def classify(input_series: np.ndarray) -> np.ndarray:
-    return await lgb_iris_runner.predict.async_run(input_series)
+@svc.api(input=bentoml.io.Text(), output=bentoml.io.NumpyNdarray())
+def predict(input_text: str):
+    return runner.predict.run([input_text])[0]
