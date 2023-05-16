ba1933f32f76bfdaf7a4357c7dcc14e4b7cbcc4a
abdellimed <57357370+abdellimed@users.noreply.github.com>
2023-05-16 09:13:50 +0200

    Update service.py

--- service.py
+++ service.py
@@ -1,11 +1,27 @@
 import numpy as np
 import bentoml
+import typing
+
+import numpy as np
+import pandas as pd
+from pydantic import BaseModel
+
+import bentoml
+from bentoml.io import JSON
+from bentoml.io import NumpyNdarray
 
 runner = bentoml.mlflow.get("comment-classifier:latest").to_runner()
-
 svc = bentoml.Service('comment-classifier', runners=[runner])
 
-
-@svc.api(input=bentoml.io.Text(), output=bentoml.io.NumpyNdarray())
-def predict(input_text: str):
-    return runner.predict.run([input_text])[0]
+class Features(BaseModel):
+    comment: str
+    # Optional field
+   # request_id: typing.Optional[int]
+    # Use custom Pydantic config for additional validation options
+   # class Config:
+      #  extra = "forbid"
+input_spec = JSON(pydantic_model=Features)
+@svc.api(input=input_spec, output=JSON())
+def predict(input_text: Features):
+    input_df = pd.DataFrame([input_text.dict()])
+    return {'res':runner.predict.run(input_df)[0]}
